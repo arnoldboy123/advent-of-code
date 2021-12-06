81f952cd716e2584fdb2af1573015018231fc208
arnold ho <tchoarnold@gmail.com>
2021-12-06 15:46:31 +0000

    completed day 6 advent of code

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like. People use them as a speed contest, interview prep, company training, university coursework, practice problems, or to challenge each other.
 
 You don't need a computer science background to participate - just a little programming knowledge and some problem solving skills will get you pretty far. Nor do you need a fancy computer; every problem has a solution that completes in at most 15 seconds on ten-year-old hardware.
+
+I have put each day's challenge and my attempt in a separate folder in my main lib folder.
+
+# Completed challenges
+- Day 6: lattern fish numbers
---
+++ lib/Day-6/day-6.rb
@@ -0,0 +1,28 @@
+init_inputs = [2,1,2,1,5,1,5,1,2,2,1,1,5,1,4,4,4,3,1,2,2,3,4,1,1,5,1,1,4,2,5,5,5,1,1,4,5,4,1,1,4,2,1,4,1,2,2,5,1,1,5,1,1,3,4,4,1,2,3,1,5,5,4,1,4,1,2,1,5,1,1,1,3,4,1,1,5,1,5,1,1,5,1,1,4,3,2,4,1,4,1,5,3,3,1,5,1,3,1,1,4,1,4,5,2,3,1,1,1,1,3,1,2,1,5,1,1,5,1,1,1,1,4,1,4,3,1,5,1,1,5,4,4,2,1,4,5,1,1,3,3,1,1,4,2,5,5,2,4,1,4,5,4,5,3,1,4,1,5,2,4,5,3,1,3,2,4,5,4,4,1,5,1,5,1,2,2,1,4,1,1,4,2,2,2,4,1,1,5,3,1,1,5,4,4,1,5,1,3,1,3,2,2,1,1,4,1,4,1,2,2,1,1,3,5,1,2,1,3,1,4,5,1,3,4,1,1,1,1,4,3,3,4,5,1,1,1,1,1,2,4,5,3,4,2,1,1,1,3,3,1,4,1,1,4,2,1,5,1,1,2,3,4,2,5,1,1,1,5,1,1,4,1,2,4,1,1,2,4,3,4,2,3,1,1,2,1,5,4,2,3,5,1,2,3,1,2,2,1,4]
+
+# At the start of each round, we actually reproduce the fishes
+def lattern_fish_numbers_reproduce(inputs)
+  inputs.each do |input|
+    inputs << 9 if input == 0
+  end
+end
+
+# We then reduce their internal timer
+def lattern_fish_numbers_reduce(inputs)
+  inputs.map do |input|
+    if input == 0
+      input = 6
+    else
+      input -= 1
+    end
+  end
+end
+
+
+# We loop this through 80 times
+80.times do
+  init_inputs = lattern_fish_numbers_reproduce(init_inputs)
+  init_inputs = lattern_fish_numbers_reduce(init_inputs)
+end
+
+p init_inputs.size
